c03b51f33d73011f2a016ac9bc16aea7e56da842
Kolleper <61471977+Kolleper@users.noreply.github.com>
2024-05-08 21:09:23 +0300

    Обновлен юнит тест

--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -4,32 +4,55 @@ namespace TestProject
 {
     public class Tests
     {
-        //проверка на вычисление площади окружности
+        private ShapeProcessor _processor;
+
+        [SetUp]
+        public void Setup()
+        {
+            _processor = new ShapeProcessor();
+        }
+
         [Test]
         public void TestCircleArea()
         {
-            var circle = Circle.CreateCircle(5);
-            Assert.That(circle.GetArea(), Is.EqualTo(78.53981633974483));
-        }
-        //проверка на вычисление площади треугольника
-        [Test]
-        public void TestTriangleArea()
-        {
-            var triangle = Triangle.CreateTriangle(3,4,5);
-            Assert.That(triangle.GetArea(), Is.EqualTo(6));
+            var circle = new Circle(5);
+            double area = _processor.CalculateArea(circle);
+            Assert.That(area, Is.EqualTo(Math.PI * 25).Within(0.001));
         }
         [Test]
         public void TestSquareArea()
         {
-            var square = Square.CreateSquare(4);
-            Assert.That(square.GetArea(), Is.EqualTo(16));
+            var square = new Square(3);
+            double area = _processor.CalculateArea(square);
+            Assert.That(area, Is.EqualTo(9));
         }
-        //проверка на прямоугольный треугольник
+
         [Test]
-        public void TestRightAngledTriangle()
+        public void TestTriangleArea()
         {
-            var triangle = Triangle.CreateTriangle(3, 4, 5);
-            Assert.IsTrue(triangle.IsRightAngled());
+            var triangle = new Triangle(3, 4, 5);
+            double area = _processor.CalculateArea(triangle);
+            Assert.That(area, Is.EqualTo(6));
+        }
+
+        [Test]
+        public void TriangleIsRightAngled()
+        {
+            var triangle = new Triangle(3, 4, 5);
+            bool isRight = _processor.IsTriangleRightAngled(triangle);
+            Assert.IsTrue(isRight);
+        }
+
+        [Test]
+        public void NotATriangleThrowsException()
+        {
+            Assert.Throws<InvalidOperationException>(() => _processor.IsTriangleRightAngled(new Circle(5)));
+        }
+
+        [Test]
+        public void TriangleWithInvalidSidesThrowsException()
+        {
+            Assert.Throws<ArgumentException>(() => new Triangle(-1, 4, 5));
         }
     }
 }
